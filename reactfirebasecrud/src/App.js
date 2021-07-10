8f7c60fe3c2010d7d10dd1a0689312c3a37a4318
Tanzim <tanzim67@gmail.com>
2021-07-11 00:28:31 +0600

    1-Adding Login registration Form

---
+++ reactfirebasecrud/src/App.js
@@ -0,0 +1,15 @@
+
+import './index.css'
+// import Login from './Login';
+import Register from './Register';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Register />
+    </div>
+  );
+}
+
+export default App;
